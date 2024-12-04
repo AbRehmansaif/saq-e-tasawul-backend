48e2c818275b85dd4fcf74cb853b51601628ee82
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2024-12-04 13:40:22 +0500

    coupon model

--- saq_e_tasawul/coupon/models.py
+++ saq_e_tasawul/coupon/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Coupon(models.Model):
+    class Meta():
+        verbose_name = _('Coupon')
+        verbose_name_plural = _('Coupons')
+    
+    product_id = models.ForeignKey(
+        'Product', on_delete=models.CASCADE, 
+        verbose_name=_('Product')
+    )
+    
+    coupon_code = models.CharField(
+        max_length=255, verbose_name=_('Coupon Code')
+    )
+    
+    quantity = models.IntegerField(
+        verbose_name=_('Quantity')
+    )
