be8ee478ef201e623bd56b9caf19993bda08040f
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2024-12-04 13:35:51 +0500

    cart model

--- saq_e_tasawul/cart/models.py
+++ saq_e_tasawul/cart/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Cart(models.Model):
+    class Meta:
+        verbose_name = _('Cart')
+        verbose_name_plural = _('Carts')
+        
+    id = models.AutoField(
+        primary_key=True
+    )
+    
+    user_id = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, 
+        related_name='cart_user_id'
+    )
+    
+    total_products = models.IntegerField(
+        default=0, verbose_name=_('Total products')
+    )
+    
+    products_id = models.ManyToManyField(
+        'products.Product', related_name='cart_products'
+    )
