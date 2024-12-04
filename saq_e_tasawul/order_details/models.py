90c65f66a487f7c5310368408fe242b296c18afa
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2024-12-04 13:46:53 +0500

    order_details

--- saq_e_tasawul/order_details/models.py
+++ saq_e_tasawul/order_details/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Order_detail(models.Model):
+    class Meta():
+        verbose_name = _('Order detail')
+        verbose_name_plural = _('Order details')
+        
+    user_id = model.ForeignKey(
+        'auth.User', on_delete=models.CASCADE, 
+        verbose_name=_('User'),
+    )
+    
+    payment_id = models.ForeignKey(
+        'payment.Payment', on_delete=models.CASCADE,
+        verbose_name=_('Payment')
+    )
+    
+    order_id = models.ForeignKey(
+        'order.Order', on_delete=models.CASCADE,
+        verbose_name=_('Order')
+    )
+    
+    total_orders = models.IntegerField(
+        verbose_name= _('Total orders')
+    )
+    
+    status = models.Boolean(
+        
+    )
+    
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('Created at')
+    )
+    
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name=_('Updated at')
+    )
