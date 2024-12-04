ac1b93b96fc4724834925e4e7eabc509c3966631
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2024-12-04 13:35:37 +0500

    category model

--- saq_e_tasawul/category/models.py
+++ saq_e_tasawul/category/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+        
+    id = models.AutoField(
+        primary_key=True
+    )
+    
+    name = models.CharField(
+        max_length=255, verbose_name=_('Category Name')
+    )
+    
+    description = models.TextField(
+        verbose_name=_('Category Description')
+    )
+    
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('Created At')
+    )
+    
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name=_('Updated At')
+    )
