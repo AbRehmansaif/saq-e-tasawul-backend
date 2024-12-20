ef5893a3010dc467df45f2df8b638f3f6f6609c2
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2024-12-20 22:46:54 +0500

    complete cart model

--- saq_e_tasawul/cart/models.py
+++ saq_e_tasawul/cart/models.py
@@ -1,25 +1,36 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from user.models import User
+from product.models import Product
 
 
 class Cart(models.Model):
     class Meta:
         verbose_name = _('Cart')
         verbose_name_plural = _('Carts')
-        
-    id = models.AutoField(
-        primary_key=True
-    )
     
-    user_id = models.ForeignKey(
-        'users.User', on_delete=models.CASCADE, 
-        related_name='cart_user_id'
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name=_('User')
     )
-    
-    total_products = models.IntegerField(
-        default=0, verbose_name=_('Total products')
+    total_products = models.PositiveBigIntegerField(
+        default=0, null=True,
+        blank=True
     )
-    
-    products_id = models.ManyToManyField(
-        'products.Product', related_name='cart_products'
+    products = models.ManyToManyField(
+        Product, through='Order'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, blank=True,
+        null=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, blank=True,
+        null=True
+    )
+    session_id = models.CharField(
+        max_length=100
+    )  # For anonymous users
+    total_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, 
+        default=0
     )
