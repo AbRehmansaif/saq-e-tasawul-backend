355f64b8ded499c6b8d407ea8c3becc12316e8b5
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2024-12-21 00:42:38 +0500

    complete payment model

--- saq_e_tasawul/payment/models.py
+++ saq_e_tasawul/payment/models.py
@@ -1,32 +1,34 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from order_details.models import Order_detail
+from order_details.models import OrderDetails
+from payment.enums import PaymentStatus
 
 
 class Payment(models.Model):
     class Meta():
         verbose_name = _('Payment')
         verbose_name_plural = _('Payments')
-        
-    id = models.AutoField(
-        primary_key=True, verbose_name = _('ID')
-    )
     
-    order_id = models.ForeignKey(
-        Order_detail.order_id, on_delete=models.CASCADE, 
-        verbose_name = _('Order ID')
+    order = models.OneToOneField(
+        OrderDetails, on_delete=models.CASCADE, 
+        related_name='payment'
     )
-    
     amount = models.ForeignKey(
-        Order_details.total_payment, on_delete=models.CASCADE,
+        OrderDetails.total_payment, on_delete=models.CASCADE,
         verbose_name = _('Amount')
     )
-    
-    user_id = models.ForeignKey(
-        User, on_delete=models.CASCADE, 
-        verbose_name = _('User ID')
-    )
-    
     status = models.BooleanField(
-        default=False, verbose_name = _('Status')
+        max_length=20,
+        choices=PaymentStatus.choices(),
+        default=PaymentStatus.PENDING.value
+    )
+    payment_method = models.CharField(
+        max_length=50, null=True, 
+        blank=True
+    )
+    transaction_id = models.CharField(
+        max_length=100, unique=True
+    )
+    payment_date = models.DateTimeField(
+        auto_now_add=True, blank=True
     )
