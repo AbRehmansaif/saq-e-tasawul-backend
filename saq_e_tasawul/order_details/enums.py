10d11fdd35ab927b639739891f988f0e7156f435
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2024-12-21 00:02:56 +0500

    order enum

---
+++ saq_e_tasawul/order_details/enums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+from django.utils.translation import gettext_lazy as _
+
+class OrderStatus(Enum):
+    PENDING    = ("pending",      _("Order has been placed and is awaiting processing."))
+    PROCESSING = ("processing",   _("Order is currently being processed."))
+    SHIPPED    = ("shipped",      _("Order has been shipped to the customer."))
+    DELIVERED  = ("delivered",    _("Order has been delivered to the customer."))
+    CANCELLED  = ("cancelled",    _("Order has been cancelled."))
