f7abb05033b714b47daf61e365281c444ccc92aa
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2025-01-08 23:04:34 +0500

    coupon serializer

---
+++ coupon/v2/serializers.py
@@ -0,0 +1,15 @@
+from django.db import models
+from coupon.models import Coupon
+
+
+class CouponSerilaizer(serializers.ModelSerializer):
+    class Meta:
+        model = Coupon
+        fields = [
+            'product', 'coupon',
+            'quantity', 'discount_type',
+            'discount_value', 'valid_from',
+            'valid_to', 'minimum_purchase', 
+            'is_active', 'usage_limit',
+            'times_used'
+        ]
